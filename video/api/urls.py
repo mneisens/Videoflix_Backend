ec977d28278dca54472c8f172a863116f3cc3b8a
michael <contact@michael-neisens.de>
2025-09-03 21:03:19 +0200

    clean code

--- video/api/urls.py
+++ video/api/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Video-Liste
     path('', views.VideoListView.as_view(), name='video_list'),
     path('<int:movie_id>/<str:resolution>/index.m3u8', views.HLSManifestView.as_view(), name='hls_manifest'),
     path('<int:movie_id>/<str:resolution>/<str:segment>', views.HLSVideoSegmentView.as_view(), name='hls_segment'),
-    
-    # Queue-Management
     path('queue/status/', views.QueueStatusView.as_view(), name='queue_status'),
     path('queue/manage/', views.QueueManagementView.as_view(), name='queue_manage'),
     path('<int:video_id>/direct/', views.DirectVideoView.as_view(), name='direct_video'),
